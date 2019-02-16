10421e5c29c8a475f052994554fb14ffa1162c04
DC <43692686+excaliburDC@users.noreply.github.com>
2019-02-16 20:14:50 +0530

    Add files via upload

---
+++ Admin/includes/footer.inc.php
@@ -0,0 +1,3 @@
+<div id="footer-wrap">
+	<p id="legal">Site Designed by  Deeppratim Chakraborty & Sinmoy Kalita BSc Comp Science 6th Sem</a>.</p>
+	</div>
---
+++ Admin/includes/logo.inc.php
@@ -0,0 +1,3 @@
+<h1>BookShop.com Admin Page</h1>
+
+	<h2> Designed by Deeppratim & Sinmoy</h2>
